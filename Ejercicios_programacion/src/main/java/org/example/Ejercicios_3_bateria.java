0489bccb938d19d08d97879acbf09223717696b5
SPIKI\ricod <denricren@alu.edu.gva.es>
2025-09-26 10:02:21 +0200

    ejrcicios 3 y 4 bateria

---
+++ Ejercicios_programacion/src/main/java/org/example/Ejercicios_3_bateria.java
@@ -0,0 +1,23 @@
+package org.example;
+
+ import java.util.Scanner;
+
+public class Ejercicios_3_bateria {
+    static void main() {
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.println("Dime un numero...");
+        int num = teclado.nextInt();
+
+        if (num == 0) {
+            System.out.println("tu numero es neutro");
+
+        }
+        if (num < 0 ) {
+            System.out.println("Tu numero es negativo");
+        }
+        if (num > 0 ) {
+            System.out.println("Tu numero es positivo");
+        }
+    }
+}
