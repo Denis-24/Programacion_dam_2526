46fc3417ca9241632743693af3b78a594c409aa0
A719\dam1 <ricorenzdenis@gmail.com>
2025-10-28 21:06:32 +0100

    terminado

--- Ejercicios_programacion/src/main/java/org/example/Contrasenya_mejorada.java
+++ Ejercicios_programacion/src/main/java/org/example/Contrasenya_mejorada.java
@@ -6,14 +6,56 @@ public class Contrasenya_mejorada {
     static void main() {
         Random aleatorio = new Random();
 
+        String caracteres = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+        int tamanyo = aleatorio.nextInt(8,13);
+
+
+        String contrasenya ="";
+
+        for (int i = 0; i<tamanyo; i++){
+            contrasenya += caracteres.charAt(aleatorio.nextInt(caracteres.length()-1));
+        }
+
+        int tamanyo_contrasenya = contrasenya.length();
+
         String caracter_max = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
         String caracter_min = "abcdefghijklmnopqrstuvwxyz";
         String caracter_num = "0123456789";
 
-
+        //numero aleatorio que va a seleccionar que caracter coger
         int numero_ale_max = aleatorio.nextInt(caracter_max.length());
         int numero_ale_min = aleatorio.nextInt(caracter_min.length());
         int numero_ale_num = aleatorio.nextInt(caracter_num.length());
+
+        //caracter escogido por el numero aleatorio
+        char letra_max = caracter_max.charAt(numero_ale_max);
+        System.out.println(letra_max);
+        char letra_min = caracter_min.charAt(numero_ale_min);
+        System.out.println(letra_min);
+        char num = caracter_num.charAt(numero_ale_num);
+        System.out.println(num);
+
+        //numero aleatorio que se va a sustituir en la contraseña
+        int posicion1_contrsenya = aleatorio.nextInt(tamanyo_contrasenya);
+        int posicion2_contrsenya = aleatorio.nextInt(tamanyo_contrasenya);
+        int posicion3_contrsenya = aleatorio.nextInt(tamanyo_contrasenya);
+
+        //letra de la contraseña que se va acambiar elegida por el numero aleatorio.
+        char letra1_sus = contrasenya.charAt(posicion1_contrsenya);
+        System.out.println(letra1_sus);
+        char letra2_sus = contrasenya.charAt(posicion2_contrsenya);
+        System.out.println(letra2_sus);
+        char letra3_sus = contrasenya.charAt(posicion3_contrsenya);
+        System.out.println(letra3_sus);
+
+        String contrasenya_paso1 = contrasenya.replace(letra1_sus,letra_max);
+        String contrasenya_paso2 = contrasenya_paso1.replace(letra2_sus,letra_min);
+        String contrasenya_paso3 = contrasenya_paso2.replace(letra3_sus,num);
+
+
+        System.out.println(contrasenya);
+        System.out.println(contrasenya_paso3);
 
         //creo un random para cada string y despues los sustituyo en un numero aleatorio de la contraseña ya creada.
     }
