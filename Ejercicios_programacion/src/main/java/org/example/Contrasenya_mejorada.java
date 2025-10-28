df4c9dd6a51569d5dcaf7dd83b1a67dd2839c267
A719\dam1 <ricorenzdenis@gmail.com>
2025-10-28 17:22:24 +0100

    contraeña majorada

---
+++ Ejercicios_programacion/src/main/java/org/example/Contrasenya_mejorada.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import java.util.Random;
+
+public class Contrasenya_mejorada {
+    static void main() {
+        Random aleatorio = new Random();
+
+        String caracter_max = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        String caracter_min = "abcdefghijklmnopqrstuvwxyz";
+        String caracter_num = "0123456789";
+
+        //creo un random para cada string y despues los sustituyo en un numero aleatorio de la contraseña ya creada.
+    }
+}
