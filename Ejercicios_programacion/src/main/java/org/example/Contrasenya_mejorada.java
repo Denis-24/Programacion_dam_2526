8a8125aea655db38eca268c7f9415b97dd048b72
A719\dam1 <ricorenzdenis@gmail.com>
2025-10-28 17:31:18 +0100

    principio de contraseña mejorada

--- Ejercicios_programacion/src/main/java/org/example/Contrasenya_mejorada.java
+++ Ejercicios_programacion/src/main/java/org/example/Contrasenya_mejorada.java
@@ -10,6 +10,11 @@ public class Contrasenya_mejorada {
         String caracter_min = "abcdefghijklmnopqrstuvwxyz";
         String caracter_num = "0123456789";
 
+
+        int numero_ale_max = aleatorio.nextInt(caracter_max.length());
+        int numero_ale_min = aleatorio.nextInt(caracter_min.length());
+        int numero_ale_num = aleatorio.nextInt(caracter_num.length());
+
         //creo un random para cada string y despues los sustituyo en un numero aleatorio de la contraseña ya creada.
     }
 }
