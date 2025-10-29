b8ff3bc5f70396167e9c402c7a71db4688e8ee5d
A719\dam1 <ricorenzdenis@gmail.com>
2025-10-29 20:18:40 +0100

    pre examen 1

--- Ejercicios_programacion/src/main/java/org/example/Btaeria_pre_examen_1.java
+++ Ejercicios_programacion/src/main/java/org/example/Btaeria_pre_examen_1.java
@@ -8,14 +8,27 @@ public class Btaeria_pre_examen_1 {
         Scanner teclado = new Scanner(System.in);
 
         int i = 0;
+        int num =0;
 
         System.out.println("Cuantos numero quieres introducir");
         int limite = teclado.nextInt();
 
         for (i=0;i>=limite;i++);{
-            System.out.println("Introduce el numero " + i );
-            int num = teclado.nextInt();
-            
+
+            try{
+                System.out.println("Introduce el numero " + i );
+                num = teclado.nextInt();
+            }catch (Exception e){
+                System.out.println("valores introducidos incorrectos");
+            }
+
+            if (num > 0){
+                System.out.println("El numero introducido es mayor que 0.");
+            } else if (num < 0) {
+                System.out.println("El numero es menor que 0.");
+            }else{
+                System.out.println("El numero es iguala 0.");
+            }
         }
     }
 }
