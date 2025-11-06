04768ade50ff35d05995b0873308375224396447
A719\dam1 <ricorenzdenis@gmail.com>
2025-11-06 17:48:22 +0100

    termiando el 5 de vectores

--- Ejercicios_programacion/src/main/java/org/example/ejercicio_5_vector.java
+++ Ejercicios_programacion/src/main/java/org/example/ejercicio_5_vector.java
@@ -11,10 +11,10 @@ public class ejercicio_5_vector {
 
         for (int i= vector.length-1;i>=0;i--){
 
-            if (i+1==vector.length){
+            if (i==vector.length-1){
                 aux=vector[i];
                 vector[i]=vector[i-1];
-            }else if (i< vector.length && i>0){
+            }else if (i>0){
                 vector[i]=vector[i-1];
             }else if (i==0){
                 vector[i]=aux;
