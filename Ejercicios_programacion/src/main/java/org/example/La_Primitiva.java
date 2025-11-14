2c922490f09d6e77c94ebce41181d4255b4d5011
A719\dam1 <ricorenzdenis@gmail.com>
2025-11-14 19:47:16 +0100

    paractica

---
+++ Ejercicios_programacion/src/main/java/org/example/La_Primitiva.java
@@ -0,0 +1,38 @@
+package org.example;
+
+import java.util.Random;
+
+public class La_Primitiva {
+    static void main() {
+
+        Random aleatorio = new Random();
+
+        int extra=aleatorio.nextInt(10);
+
+        int vector1[] = new int[6];
+
+        for (int i=0;i<vector1.length;i++){
+            vector1[i]=aleatorio.nextInt(1,50);
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
