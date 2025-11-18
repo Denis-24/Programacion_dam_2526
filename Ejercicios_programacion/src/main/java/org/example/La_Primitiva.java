eb5757bc26bb6726641bf2deb5d473ebd59fe921
SPIKI\ricod <denricren@alu.edu.gva.es>
2025-11-18 01:34:42 +0100

    mas cosas en el sorteo

--- Ejercicios_programacion/src/main/java/org/example/La_Primitiva.java
+++ Ejercicios_programacion/src/main/java/org/example/La_Primitiva.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -11,22 +12,34 @@ public class La_Primitiva {
         Random aleatorio = new Random();
 
         int extra=aleatorio.nextInt(10);
-
         int vector1[] = new int[6];
+        String formato = "\\d{1,2}-\\d{1,2}-\\d{1,2}-\\d{1,2}-\\d{1,2}-\\d{1,2}/\\d{1}";
+        int Complemento = aleatorio.nextInt(1,50);
+        int reintegro = aleatorio.nextInt(0,9);
 
         System.out.println("Introduce los datos de tu boleto:");
         String resultado= teclado.next();
 
-        resultado.split("\\d{1,2}\\d{1,2}\\d{1,2}\\d{1,2}\\d{1,2}-\\d{1,2}/\\d{1}");
+        if (resultado.matches(formato)==true){
+            System.out.println("verdadero");
+        }else{
+            System.out.println("falso");
+        }
 
-
-
+        for (int i=0;i<vector1.length;i++){
+            vector1[i]=resultado.charAt(i);
+        }
+        System.out.println(Arrays.toString(vector1));
 
 
         for (int i=0;i<vector1.length;i++){
             vector1[i]=aleatorio.nextInt(1,50);
         }
 
+        System.out.println("Ha salido:");
+        System.out.println(Arrays.toString(vector1));
+        System.out.println("complementario = " + Complemento);
+        System.out.println("Reintegro: " + reintegro);
 
 
 
