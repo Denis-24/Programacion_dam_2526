60a8b1f50060d7ec281c40802296427ea81c0e16
SPIKI\ricod <denricren@alu.edu.gva.es>
2025-11-23 20:04:37 +0100

    practica spectrum

--- Ejercicios_programacion/src/main/java/org/example/ZX_Spectrum.java
+++ Ejercicios_programacion/src/main/java/org/example/ZX_Spectrum.java
@@ -11,11 +11,18 @@ public class ZX_Spectrum {
 
         int alto=0;
         int ancho=0;
+        int contador=0;
+        String test = "a";
+        String test2 = "";
 
         System.out.println("****** VALIDADOR DE COMPATIBILIDAD ZX SPECTRUM ******");
         System.out.println("Introduce la resolucion de tu pantalla (ancho x alto)...");
 
         do {
+            contador++;
+            if (contador>1){
+                System.out.println("Valores introducidos incorrectos, inetnto num " + (contador-1));
+            }
             System.out.println("Ancho:");
             ancho = teclado.nextInt();
             System.out.println("Alto");
@@ -38,31 +45,53 @@ public class ZX_Spectrum {
             System.out.println();
         }
 
+        externo:
+        for (String fila[] : matriz){
+            interno:
+            for (int x = 0, j=0; x < fila.length-1; x++ , j++) {
 
+                test=fila[0];
+                test2=test;
+                boolean hay = Arrays.asList(test2).contains(fila[x]);
+                if (!hay){
+                    test2=fila[x];
+                }
 
+                if (ancho==16) {
+                    if (j%8==0){
+                        test=fila[j];
+                        test2=test;
+                        hay = Arrays.asList(test2).contains(fila[x]);
+                        if (!hay){
+                            test2=fila[x];
+                        }
+                    }
+                } else if (ancho==24) {
+                    
+                } else if (ancho==32) {
+                    
+                } else if (ancho==40) {
+                    
+                } else if (ancho==48) {
+                    
+                }
 
+                if (fila[x].equals(test2)==false){
+                    contador++;
+                }
 
+                if (contador>0){
+                    System.out.println("No es compatible");
+                    System.out.println(contador);
+                    System.out.println(test);
+                    System.out.println(test2);
+                    break externo;
+                }else{
+                    System.out.println("Es compatible...");
+                    break externo;
+                }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+            }
+        }
     }
 }
